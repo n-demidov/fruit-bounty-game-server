9755ac2ad4cce79af13f77d0f1001f5f83027bb0
n-demidov <you@example.com>
2018-06-22 22:07:57 +0600

    Add empty first test.

---
+++ fruit-bounty-server/src/test/java/com/demidovn/fruitbounty/server/integrationtests/AaFirstTest.java
@@ -0,0 +1,11 @@
+package com.demidovn.fruitbounty.server.integrationtests;
+
+import org.junit.Test;
+
+public class AaFirstTest extends AbstractAuthMockedTest {
+
+  @Test
+  public void justUpServer() {
+  }
+
+}
