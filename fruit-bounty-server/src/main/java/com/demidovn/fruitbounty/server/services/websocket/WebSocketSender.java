a071ccc5cd5cdef1031575cf3e5f48c98beb76f3
Nikita Demidov <ndemidov@inno.tech>
2024-11-19 11:16:04 +0300

    Fix websocket encoding

--- fruit-bounty-server/src/main/java/com/demidovn/fruitbounty/server/services/websocket/WebSocketSender.java
+++ fruit-bounty-server/src/main/java/com/demidovn/fruitbounty/server/services/websocket/WebSocketSender.java
@@ -16,23 +16,36 @@ public class WebSocketSender {
     private SimpMessageSendingOperations messagingTemplate;
 
     public void sendToAll(Object payload) {
-        messagingTemplate.convertAndSend(AppConfigs.WS_TOPIC_NAME, payload);
+        messagingTemplate.convertAndSend(
+            AppConfigs.WS_TOPIC_NAME,
+            payload,
+            createMessageHeaders());
     }
 
     public void sendToSession(String sessionId, Object payload) {
         messagingTemplate.convertAndSendToUser(
-                sessionId,
-                AppConfigs.WS_USER_QUEUE_NAME,
-                payload,
-                createMessageHeaders(sessionId));
+            sessionId,
+            AppConfigs.WS_USER_QUEUE_NAME,
+            payload,
+            createMessageHeaders(sessionId));
     }
 
     private MessageHeaders createMessageHeaders(String sessionId) {
-        SimpMessageHeaderAccessor headerAccessor = SimpMessageHeaderAccessor.create(SimpMessageType.MESSAGE);
+        SimpMessageHeaderAccessor headerAccessor = createSimpMessageHeaderAccessor();
         headerAccessor.setSessionId(sessionId);
+        return headerAccessor.getMessageHeaders();
+    }
+
+    private MessageHeaders createMessageHeaders() {
+        SimpMessageHeaderAccessor headerAccessor = createSimpMessageHeaderAccessor();
+        return headerAccessor.getMessageHeaders();
+    }
+
+    private static SimpMessageHeaderAccessor createSimpMessageHeaderAccessor() {
+        SimpMessageHeaderAccessor headerAccessor = SimpMessageHeaderAccessor.create(SimpMessageType.MESSAGE);
         headerAccessor.setLeaveMutable(true);
         headerAccessor.setContentType(MimeType.valueOf("application/json;charset=UTF-16"));
-        return headerAccessor.getMessageHeaders();
+        return headerAccessor;
     }
 
 }
